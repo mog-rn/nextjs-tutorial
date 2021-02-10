3494620a2c11b34a7dd3465f1c12f9c281c27342
Mogakamo <mogaka.amo254@gmail.com>
2021-02-10 21:15:43 +0300

    Commit

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,5 +3,18 @@ import Layout, { siteTitle } from '../Components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
-  
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a simple website - you'll be building a site like this on{' '}
+          <a href="https://next.js/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
