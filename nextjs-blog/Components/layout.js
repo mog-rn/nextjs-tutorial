6733c063f10ea0d5844bc4e3beb9d7e896f58d4f
Mogakamo <mogaka.amo254@gmail.com>
2021-02-10 20:57:48 +0300

    Commit

--- nextjs-blog/Components/layout.js
+++ nextjs-blog/Components/layout.js
@@ -4,6 +4,29 @@ import Link from 'next/link'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
+const name = 'Amos Mogaka'
+export const siteTitle = 'Next.js Sample Webiste'
+
+
 export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta 
+                    name="description"
+                    content="Learn how to build a personal website using Next.js" 
+                />
+                <meta 
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} 
+                />
+                <meta
+                    
+                />
+            </Head>
+            {children}</div>
+    )
 }
